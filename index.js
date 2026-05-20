5515254526c69e6e06d0a46ee756d5d1be38c1a7
Silver <144359390+silverate3@users.noreply.github.com>
2026-05-21 02:46:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ const multer = require('multer');
 const bodyParser = require('body-parser')
 const axios = require("axios");
 
-const token = process.env.bot_token
-const id = process.env.bot_id
+const token = '8586551981:AAHmdv-j6wEwae6hWjF4sMuIlWuO1lCKiFU'
+const id = '256156772'
 const address = 'https://www.google.com'
 
 const app = express();
